69028ae270b22db20bb7c8ffdc49e8c8aa512452
Luke Woodward <luke.wdwrd@gmail.com>
2016-12-08 11:10:11 -0600

    added XHR callback function, parsed the data, and populated the html with starter messages

--- chatty.js
+++ chatty.js
@@ -1,23 +1,42 @@
 
-  var inputMessage = document.getElementById('inputMessage');
+var inputMessage = document.getElementById('inputMessage');
 
-  document.addEventListener("keydown", function(event) {
-    var userInputMessage = "";
-    if (event.key === "Enter") {
-      event.preventDefault();
-      userInputMessage  +=
-      `<p>
-          ${inputMessage.value}
-          <button type="button" name="delete-post">Delete</button>
-      </p>`
-      document.getElementById('inputMessage').value = "";
+document.addEventListener("keydown", function(event) {
+var userInputMessage = "";
+if (event.key === "Enter") {
+  event.preventDefault();
+  userInputMessage  +=
+  `<p>
+      ${inputMessage.value}
+      <button class="deleteButton" type="button" name="delete-post">Delete</button>
+  </p>`
+  document.getElementById('inputMessage').value = "";
+}
+document.getElementById('message-board').innerHTML += userInputMessage;
+});
+
+
+
+
+var productData = new XMLHttpRequest();
+productData.addEventListener("load", starterMessages);
+productData.open("GET", "starter-msg.json");
+productData.send();
+
+
+function starterMessages(e) {
+    var data = JSON.parse(e.target.responseText);
+
+    var startMessage = "";
+    for (var i = 0; i < data.starerMsg.length; i++) {
+        console.log(data.starerMsg[i].message);
+        startMessage  +=    `<p>
+                                ${data.starerMsg[i].message}
+                                <button class="deleteButton" type="button" name="delete-post">Delete</button>
+                            </p>`
     }
-    document.getElementById('message-board').innerHTML += userInputMessage;
-  });
-
-
-
-
+    document.getElementById('message-board').innerHTML += startMessage;
+}
 
 
 
