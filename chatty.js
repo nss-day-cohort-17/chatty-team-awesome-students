4da9ff835cc8e3fe24d6cd4c76781a963eb05a43
Jared Fuller <jaredshane@gmail.com>
2016-12-08 10:27:08 -0600

    fixed to get multiple messages to append and input field will clear upon hitting enter

--- chatty.js
+++ chatty.js
@@ -2,15 +2,17 @@
   var inputMessage = document.getElementById('inputMessage');
 
   document.addEventListener("keydown", function(event) {
+    var userInputMessage = "";
     if (event.key === "Enter") {
       event.preventDefault();
-      document.getElementById('message-board').innerHTML =
+      userInputMessage  +=
       `<p>
           ${inputMessage.value}
           <button type="button" name="delete-post">Delete</button>
       </p>`
-
+      document.getElementById('inputMessage').value = "";
     }
+    document.getElementById('message-board').innerHTML += userInputMessage;
   });
 
 
