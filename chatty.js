ab9879c8a8cf73276fd9a44d7ef12d19d92a9056
Jared Fuller <jaredshane@gmail.com>
2016-12-08 09:53:39 -0600

    fixed js because of changing of one id and another id missing during a merge.

--- chatty.js
+++ chatty.js
@@ -3,6 +3,7 @@
 
   document.addEventListener("keydown", function(event) {
     if (event.keyCode === 13) {
+      event.preventDefault();
       document.getElementById('message-board').innerHTML = inputMessage.value;
     }
   });
